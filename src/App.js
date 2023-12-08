3e85385503861230e48e0066d72f2091a0436386
Ali Bertay SOLAK <54456222+alibertay@users.noreply.github.com>
2023-12-08 13:53:57 +0300

    Button Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+    const [buttonText, setButtonText] = useState("Connect Wallet");
+    const [buttonAction, setButtonAction] = useState(() => connectWallet)
+
+    function connectWallet() {
+        setButtonText("Swap");
+        setButtonAction(() => swap);
+    }
+
+    function swap() {
+        alert("Swapped");
+    }
+
     return (
         <div className="App">
             <div className="centered-div">
-                
+                <button onClick={buttonAction}>
+                    <span className="button-text">{buttonText}</span>
+                </button>
             </div>
         </div>
     );
